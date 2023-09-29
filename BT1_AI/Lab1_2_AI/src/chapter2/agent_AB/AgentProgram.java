2da3b7d0859ee363f4372b39be69fe1e21b832db
Ngô Thanh Tân <145730565+ngothanhtan21130529@users.noreply.github.com>
2023-09-29 23:02:09 +0700

    úp folder lab_1 lần 1

---
+++ BT1_AI/Lab1_2_AI/src/chapter2/agent_AB/AgentProgram.java
@@ -0,0 +1,18 @@
+package chapter2.agent_AB;
+
+import chapter2.agent_AB.Environment.LocationState;
+
+public class AgentProgram {
+
+	public Action execute(Percept p) {// location, status
+		if (p.getLocationState() == LocationState.DIRTY) {
+			return Environment.SUCK_DIRT;
+		} else if (p.getAgentLocation() == Environment.LOCATION_A) {
+			return Environment.MOVE_LEFT;
+		} else if (p.getAgentLocation() == Environment.LOCATION_B) {
+			return Environment.MOVE_RIGHT;
+		}
+		return NoOpAction.NO_OP;
+
+	}
+}
---
+++ BT1_AI/Lab1_AI/src/chapter2/agent_AB/Test.java
@@ -0,0 +1,5 @@
+package chapter2.agent_AB;
+
+public class Test {
+
+}
